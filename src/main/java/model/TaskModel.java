a3b45eb8e001bb4692d28fb462e602f9a5db6dd8
Eric Adam <eadamfs.programador@gmail.com>
2022-07-16 21:52:20 -0300

    Criada a class TaskModel

---
+++ src/main/java/model/TaskModel.java
@@ -0,0 +1,106 @@
+package model;
+
+import java.util.Date;
+
+public class TaskModel {
+
+    private int id;
+    private int idProject;
+    private String name;
+    private String description;
+    private Boolean isCompleted;
+    private String notes;
+    private Date deadline;
+    private Date createdAt;
+    private Date updatedAt;
+
+    public TaskModel(int id, int idProject, String name, String description, Boolean isCompleted, String notes, Date deadline, Date createdAt, Date updatedAt) {
+        this.id = id;
+        this.idProject = idProject;
+        this.name = name;
+        this.description = description;
+        this.isCompleted = isCompleted;
+        this.notes = notes;
+        this.deadline = deadline;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getIdProject() {
+        return idProject;
+    }
+    public void setIdProject(int idProject) {
+        this.idProject = idProject;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Boolean getCompleted() {
+        return isCompleted;
+    }
+    public void setCompleted(Boolean completed) {
+        isCompleted = completed;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
+
+    public Date getDeadline() {
+        return deadline;
+    }
+    public void setDeadline(Date deadline) {
+        this.deadline = deadline;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Date getUpdatedAt() {
+        return updatedAt;
+    }
+    public void setUpdatedAt(Date updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    @Override
+    public String toString() {
+        return "TaskModel{" +
+                "id=" + id +
+                ", idProject=" + idProject +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", isCompleted=" + isCompleted +
+                ", notes='" + notes + '\'' +
+                ", deadline=" + deadline +
+                ", createdAt=" + createdAt +
+                ", updatedAt=" + updatedAt +
+                '}';
+    }
+}
