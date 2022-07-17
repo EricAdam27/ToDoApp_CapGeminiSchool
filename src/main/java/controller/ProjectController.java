357fcaf4c8adff4da2b2e57978b2317e3f73eb83
Eric Adam <eadamfs.programador@gmail.com>
2022-07-17 10:09:03 -0300

    Criada a class ProjectController

---
+++ src/main/java/controller/ProjectController.java
@@ -0,0 +1,119 @@
+package controller;
+
+import model.ProjectModel;
+import util.ConnectionFactory;
+
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProjectController {
+
+    public void save(ProjectModel project) {
+
+        String sql = "INSERT INTO projects(" +
+                "name, " +
+                "description, " +
+                "createdAt, " +
+                "updatedAt) VALUES (?, ?, ?, ?)";
+
+        Connection connection = null;
+        PreparedStatement statement = null;
+
+        try {
+            connection = ConnectionFactory.getConnection();
+            statement = connection.prepareStatement(sql);
+            statement.setString(1, project.getName());
+            statement.setString(2, project.getDescription());
+            statement.setDate(3, new Date(project.getCreatedAt().getTime()));
+            statement.setDate(4, new Date(project.getUpdatedAt().getTime()));
+            statement.execute();
+        } catch (Exception e) {
+            throw new RuntimeException("Erro ao salvar projeto "+ e.getMessage(), e);
+        } finally {
+            ConnectionFactory.closeConnection(connection, statement);
+        }
+    }
+
+    public void update(ProjectModel project) {
+
+        String sql = "UPDATE projects SET " +
+                "name = ?, " +
+                "description = ? ," +
+                "createdAt = ?, " +
+                "updatedAt = ? WHERE id = ?";
+
+        Connection connection = null;
+        PreparedStatement statement = null;
+
+        try {
+            connection = ConnectionFactory.getConnection();
+            statement = connection.prepareStatement(sql);
+            statement.setString(1, project.getName());
+            statement.setString(2, project.getDescription());
+            statement.setDate(3, new Date(project.getCreatedAt().getTime()));
+            statement.setDate(4, new Date(project.getUpdatedAt().getTime()));
+            statement.setInt(5, project.getId());
+            statement.execute();
+        } catch (Exception e) {
+            throw new RuntimeException("Erro ao atualizar projeto "+ e.getMessage(), e);
+        } finally {
+            ConnectionFactory.closeConnection(connection, statement);
+        }
+    }
+
+    public void deleteById(int idProject) {
+
+        String sql = "DELETE FROM projects WHERE id = ?";
+
+        Connection connection = null;
+        PreparedStatement statement = null;
+
+        try {
+            connection = ConnectionFactory.getConnection();
+            statement = connection.prepareStatement(sql);
+            statement.setInt(1, idProject);
+            statement.execute();
+        } catch (Exception e) {
+            throw new RuntimeException("Erro ao deletar projeto "+ e.getMessage(), e);
+        } finally {
+            ConnectionFactory.closeConnection(connection, statement);
+        }
+    }
+
+    public List<ProjectModel> getAll(){
+
+        String sql = "SELECT * FROM projects";
+
+        Connection connection = null;
+        PreparedStatement statement = null;
+        ResultSet resultSet = null;
+
+        List<ProjectModel> projects = new ArrayList<>();
+
+        try {
+            connection = ConnectionFactory.getConnection();
+            statement = connection.prepareStatement(sql);
+            resultSet = statement.executeQuery();
+
+            while (resultSet.next()) {
+                ProjectModel project = new ProjectModel();
+                project.setName(resultSet.getString("name"));
+                project.setDescription(resultSet.getString("description"));
+                project.setCreatedAt(resultSet.getDate("createdAt"));
+                project.setUpdatedAt(resultSet.getDate("updatedAt"));
+
+                projects.add(project);
+            }
+        } catch (Exception e) {
+            throw new RuntimeException("Erro ao buscar projetos "+ e.getMessage(), e);
+        } finally {
+            ConnectionFactory.closeConnection(connection, statement, resultSet);
+        }
+
+        return projects;
+    }
+}
