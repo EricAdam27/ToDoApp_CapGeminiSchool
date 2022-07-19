e949eb02b94ec908a62968fb1ade7617657ea9a1
Eric Adam <eadamfs.programador@gmail.com>
2022-07-19 20:57:26 -0300

    Criada a class MainScreen no package view

---
+++ src/main/java/view/MainScreen.java
@@ -0,0 +1,21 @@
+package view;
+
+import javax.swing.*;
+
+public class MainScreen {
+    private JPanel panel1;
+    private JList jListsProjects;
+    private JPanel jPanelToolBar;
+    private JLabel jLabelToolBarTitle;
+    private JLabel jLabelToolBarSubTitle;
+    private JPanel jPanelProjects;
+    private JLabel jLabelProjectsTitle;
+    private JLabel jLabelProjectsAdd;
+    private JPanel jPanelTasks;
+    private JLabel jLabelTasksTitle;
+    private JLabel jLabelTasksAdd;
+    private JPanel jPanelListsProjects;
+    private JPanel jPanelListsTasks;
+    private JLabel jLabelEmptyListTitle;
+    private JLabel jLabelEmptyListSubTitle;
+}
